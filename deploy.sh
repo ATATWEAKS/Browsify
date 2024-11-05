5b2f7d64b369d9ee811c3211f7e713c35d502b38
Nesip <nesiptrnc@gmail.com>
2024-11-05 17:46:59 +0100

    aaaaaaaa

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-if (( $# != 1 )); then
+if (( $# != 1                                                                                                                                       ); then
     >&2 echo "usage: deploy <deploy-branch>"
     exit
 fi
